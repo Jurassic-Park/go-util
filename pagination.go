485037a0f466434458bdccd5626a8adae189b11d
root <1723877247@qq.com>
2020-05-06 16:10:00 +0800

    add

--- pagination.go
+++ pagination.go
@@ -1,11 +1,22 @@
 package util
 
-// GetPage get page parameters
-func GetPage(page int, pageSize int) int {
+import "math"
+
+// GetPage get page parameters, returns pagenum, pagesize
+func GetPage(page int, pageSize int) (int, int) {
+	if pageSize == 0 {
+		pageSize = 20
+	}
+
 	result := 0
 	if page > 0 {
 		result = (page - 1) * pageSize
 	}
 
-	return result
+	return result, pageSize
+}
+
+// GetTotalPage get total page by totalCount and pageSize
+func GetTotalPage(totalCount int, pageSize int) int {
+	return int(math.Ceil(float64(totalCount) / float64(pageSize)))
 }
